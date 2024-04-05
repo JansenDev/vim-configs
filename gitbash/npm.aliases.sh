cb306aab1cf1b26a9dfe104609d8994a0cdcabac
Jhonatan Segura Galindo <71657821+JansenDev@users.noreply.github.com>
2024-04-04 20:37:42 -0500

    add. git bash config

---
+++ gitbash/npm.aliases.sh
@@ -0,0 +1,10 @@
+alias nr="npm run"
+alias nrb="npm run build"
+alias nrbl="npm run build:lib"
+alias nrt="npm run test"
+alias nrtw="npm runtest:watch"
+alias nrd="npm run dev"
+alias nrsd="npm run start:dev"
+
+alias ni="npm install"
+alias nu="npm uninstall"
